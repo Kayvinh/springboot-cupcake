2569930f79933798450917bcb5987ac37dc5c31b
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-11-13 13:45:14 -0800

    cors

--- src/main/java/com/kevthedev/springboot/cupcake/CupcakeController.java
+++ src/main/java/com/kevthedev/springboot/cupcake/CupcakeController.java
@@ -1,12 +1,14 @@
 package com.kevthedev.springboot.cupcake;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kevthedev.springboot.cupcake.service.CupcakeService;
@@ -14,6 +16,7 @@ import com.kevthedev.springboot.cupcake.model.Cupcake;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class CupcakeController {
 
     private final CupcakeService cupcakeService;
--- src/main/java/com/kevthedev/springboot/cupcake/SpringbootCupcakeApplication.java
+++ src/main/java/com/kevthedev/springboot/cupcake/SpringbootCupcakeApplication.java
@@ -3,6 +3,7 @@ package com.kevthedev.springboot.cupcake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringbootCupcakeApplication {
 
